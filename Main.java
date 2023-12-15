51f91fa22eaa7a29fcfb405973c7a2a13112ea11
bosiwa1 <117687360+bosiwa1@users.noreply.github.com>
2023-12-15 11:29:15 +0000

    aad main

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+import java.util.*; 
+public class Main
+{
+    // instance variables - replace the example below with your own
+  
+
+   
+    public  static void Main(String[] args )
+    {
+        System.out.println ("hello ") ;
+      
+    }
+
+    
+}
