c69aa2223f29b32b3e3c65c61aa968458735f52b
bosiwa1 <117687360+bosiwa1@users.noreply.github.com>
2023-12-15 11:14:58 +0000

    add work

---
+++ JavaSersion.java
@@ -0,0 +1,8 @@
+public class JavaSersion {
+     public static void main(String[] args) {
+         System.out.println(" hello word ");
+         Student student = new Student(22,"didier lokamba " );
+         student.display();
+
+    }
+}
